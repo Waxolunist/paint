4e656bb9261dda19bb3650c18edd68a08b310e4f
Christian Sterzl <christian.sterzl@senacor.com>
2020-04-28 22:32:39 +0200

    Update build
    Signed-off-by: Christian Sterzl <christian.sterzl@senacor.com>

--- src/components/my-app.js
+++ src/components/my-app.js
@@ -70,12 +70,9 @@ class MyApp extends connect(store)(LitElement) {
     // To force all event listeners for gestures to be passive.
     // See https://www.polymer-project.org/2.0/docs/devguide/gesture-events#use-passive-gesture-listeners
     setPassiveTouchGestures(true);
-    try {
-      window.screen.orientation.lock('portrait');
-    }
-    catch(err) {
+    window.screen.orientation.lock('portrait').catch(err => {
       console.log('Ignore this error: ' + err.message);
-    }
+    });
   }
 
   _firstRendered() {
